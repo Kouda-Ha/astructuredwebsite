7979e9f57f7abf09f6d7033070314a6ca2b21417
Kouda-Ha <Kouda-Ha@users.noreply.github.com>
2021-02-13 15:12:00 +0000

    Added database connection and got title of all book(s)

---
+++ database.php
@@ -0,0 +1,8 @@
+<?php
+
+$database_host = "localhost";
+$database_name = "creditcard";
+$database_username = "root";
+$database_password = "";
+
+?>
--- index.php
+++ index.php
@@ -1,1 +1,34 @@
-<!-- Index page -->
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+<meta charset="UTF-8"> 
+<title>Black Books</title>
+</head>
+<body>
+<?php
+include 'database.php';
+$link = mysqli_connect($database_host, $database_username, $database_password,$database_name);
+if(!$link) die('failed');
+
+
+
+
+$sql = "SELECT * FROM book";
+	$result = $link->query($sql);
+	if ($result->num_rows > 0) { 
+		while($row = $result->fetch_assoc()) {  
+			echo ("Title: ".$row["title"]."<br>"); 
+		} 
+	}
+    else {
+		echo "0 results"; 
+	} 
+
+
+?>
+
+
+
+</body>
+</html>
+

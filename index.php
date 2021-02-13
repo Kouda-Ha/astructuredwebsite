9217ca6ff90bb4728c30c597a6df4134cdbd9232
Kouda-Ha <Kouda-Ha@users.noreply.github.com>
2021-02-13 16:34:52 +0000

    Add styles and import all books to index page

--- index.php
+++ index.php
@@ -3,11 +3,13 @@
 <head>
 <meta charset="UTF-8"> 
 <title>Black Books</title>
+<link type="text/css" href="css/style.css" rel="stylesheet">
 </head>
+
 <body>
 <?php
 include 'database.php';
-$link = mysqli_connect($database_host, $database_username, $database_password,$database_name);
+$link = mysqli_connect($database_host, $database_username, $database_password, $database_name);
 if(!$link) die('failed');
 
 
@@ -17,7 +19,14 @@ $sql = "SELECT * FROM book";
 	$result = $link->query($sql);
 	if ($result->num_rows > 0) { 
 		while($row = $result->fetch_assoc()) {  
-			echo ("Title: ".$row["title"]."<br>"); 
+			?> 
+				<div class="bookBlock">
+					<img class="bookImage" src="images/books/<?= $row['image'] ?>">
+					<div class="bookTitle"><?= $row['title'] ?></div>
+					<div class="bookDescription"><?= $row['description'] ?></div>
+					<div class="bookPrice">£<?= $row['price']/100 ?></div>				
+				</div>		
+			<?php
 		} 
 	}
     else {
